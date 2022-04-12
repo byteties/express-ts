0ece1723ad58919fa879be6d973113490b25570d
Chayangkoon <chayangkoondokhom@gmail.com>
2022-04-12 15:38:56 +0700

    fix : refactor logic

--- src/controller/answer.ts
+++ src/controller/answer.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 const get = async(req:Request,res:Response) =>{
   const { params } = req
   const { id } = params
-  const index = Number(id) - 1
+  const index = Number(id)
   const answerList = ['Heaven','Earth','Heart']
 
   const answer = answerList[index]
